4fcb0583ffd56a721d6dea005ca85f4a6b7d2d0a
qianjunfan <qianjunfan0@gmail.com>
2025-12-09 23:36:35 +0800

    new

--- pico.c
+++ pico.c
@@ -1,140 +1,123 @@
 #include <X11/Xlib.h>
 #include <X11/keysym.h>
-#include <X11/XKBlib.h> 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
-
-
+#include <X11/XKBlib.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <signal.h>
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
-#define SUPER_MASK Mod4Mask
+#define MOD_MASK Mod4Mask
+#define MOUSE_MASK (ButtonPressMask|ButtonReleaseMask|PointerMotionMask)
+#define CLEANMASK(mask) (mask & ~(numlockmask|LockMask) & (ShiftMask|ControlMask|Mod1Mask|Mod2Mask|Mod3Mask|Mod4Mask|Mod5Mask))
 
+static Display *dpy;
+static Window root;
+static int screen;
+static int sw, sh;
+static unsigned int numlockmask = 0;
 
-static const unsigned int ignore_mods[] = {
-    0,
-    LockMask,
-    Mod2Mask,
-    Mod3Mask,
-    Mod5Mask,
-    LockMask | Mod2Mask,
-    LockMask | Mod3Mask,
-    LockMask | Mod5Mask,
-    Mod2Mask | Mod3Mask,
-    Mod2Mask | Mod5Mask,
-    Mod3Mask | Mod5Mask,
-    LockMask | Mod2Mask | Mod3Mask,
-    LockMask | Mod2Mask | Mod5Mask,
-    Mod2Mask | Mod3Mask | Mod5Mask,
-    LockMask | Mod2Mask | Mod3Mask | Mod5Mask
-};
-
-void launch_xterm() {
+void spawn(const char *cmd) {
     if (fork() == 0) {
+        if (dpy) close(ConnectionNumber(dpy));
         setsid();
-        execlp("xterm", "xterm", NULL);
-        perror("Failed to launch xterm");
-        _exit(EXIT_FAILURE);
+        execlp(cmd, cmd, NULL);
+        _exit(0);
     }
 }
 
-void grab_input(Display *dpy)
-{
-    unsigned int i;
-    KeyCode keycode_f1 = XKeysymToKeycode(dpy, XK_F1);
-    
-   
-    for (i = 0; i < sizeof(ignore_mods) / sizeof(ignore_mods[0]); i++) {
-        unsigned int mod = SUPER_MASK | ignore_mods[i];
-        
-        if (keycode_f1) {
-             XGrabKey(dpy, keycode_f1, mod,
-                 DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
-        }
+void updatenumlockmask(void) {
+    unsigned int i, j;
+    XModifierKeymap *modmap;
+    numlockmask = 0;
+    modmap = XGetModifierMapping(dpy);
+    for (i = 0; i < 8; i++)
+        for (j = 0; j < modmap->max_keypermod; j++)
+            if (modmap->modifiermap[i * modmap->max_keypermod + j] == XKeysymToKeycode(dpy, XK_Num_Lock))
+                numlockmask = (1 << i);
+    XFreeModifiermap(modmap);
+}
 
-       
-        XGrabButton(dpy, 1, mod, DefaultRootWindow(dpy), True,
-                ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
-        
-       
-        XGrabButton(dpy, 3, mod, DefaultRootWindow(dpy), True,
-                ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
+void grabkeys(void) {
+    updatenumlockmask();
+    unsigned int i, j;
+    unsigned int modifiers[] = { 0, LockMask, numlockmask, numlockmask|LockMask };
+    KeyCode k1 = XKeysymToKeycode(dpy, XK_F1);
+    KeyCode k2 = XKeysymToKeycode(dpy, XK_F2);
+
+    XUngrabKey(dpy, AnyKey, AnyModifier, root);
+
+    for (j = 0; j < 4; j++) {
+        if (k1) XGrabKey(dpy, k1, MOD_MASK | modifiers[j], root, True, GrabModeAsync, GrabModeAsync);
+        if (k2) XGrabKey(dpy, k2, MOD_MASK | modifiers[j], root, True, GrabModeAsync, GrabModeAsync);
+        XGrabButton(dpy, 1, MOD_MASK | modifiers[j], root, True, MOUSE_MASK, GrabModeAsync, GrabModeAsync, None, None);
+        XGrabButton(dpy, 3, MOD_MASK | modifiers[j], root, True, MOUSE_MASK, GrabModeAsync, GrabModeAsync, None, None);
     }
 }
 
-int main(void)
-{
-    Display * dpy;
-    XWindowAttributes attr;
-    XButtonEvent start;
+int main(void) {
     XEvent ev;
+    XWindowAttributes attr;
+    XButtonEvent start = {0};
 
-   
-    if(!(dpy = XOpenDisplay(NULL))) {
-        fprintf(stderr, "Cannot open display. Are you running inside X?\n");
-        return 1;
-    }
-
-   
-   
-   
-    XSetErrorHandler(NULL);
-    if (XSelectInput(dpy, DefaultRootWindow(dpy),
-                     SubstructureRedirectMask | SubstructureNotifyMask) & BadAccess)
-    {
-        fprintf(stderr, "Error: Another window manager is already running.\n");
-        fprintf(stderr, "Try running inside Xephyr: \n");
-        fprintf(stderr, "  Xephyr -br -ac -noreset -screen 800x600 :1 &\n");
-        fprintf(stderr, "  DISPLAY=:1 ./your_wm\n");
-        XCloseDisplay(dpy);
-        return 1;
-    }
+    if (!(dpy = XOpenDisplay(NULL))) return 1;
     
-    launch_xterm();
-    grab_input(dpy);
+    struct sigaction sa;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_NOCLDSTOP | SA_NOCLDWAIT | SA_RESTART;
+    sa.sa_handler = SIG_IGN;
+    sigaction(SIGCHLD, &sa, NULL);
 
-    start.subwindow = None;
+    screen = DefaultScreen(dpy);
+    root = RootWindow(dpy, screen);
+    sw = DisplayWidth(dpy, screen);
+    sh = DisplayHeight(dpy, screen);
 
-   
-    for(;;)
-    {
+    XSelectInput(dpy, root, SubstructureRedirectMask | SubstructureNotifyMask);
+    
+    grabkeys();
+    spawn("xterm");
+
+    for (;;) {
         XNextEvent(dpy, &ev);
-        
-        if(ev.type == KeyPress)
-        {
-           
-           
+
+        if (ev.type == KeyPress) {
             KeySym keysym = XkbKeycodeToKeysym(dpy, ev.xkey.keycode, 0, 0);
-
-            if (keysym == XK_F1 && (ev.xkey.state & SUPER_MASK)) {
-                
-                 if(ev.xkey.subwindow != None) {
-                    XRaiseWindow(dpy, ev.xkey.subwindow);
-                    XSetInputFocus(dpy, ev.xkey.subwindow, RevertToParent, CurrentTime);
-                 }
+            if (CLEANMASK(ev.xkey.state) == MOD_MASK) {
+                if (keysym == XK_F1) {
+                    spawn("xterm");
+                } else if (keysym == XK_F2) {
+                    if (ev.xkey.subwindow != None) {
+                         XDestroyWindow(dpy, ev.xkey.subwindow);
+                    } else {
+                        Window w;
+                        int revert;
+                        XGetInputFocus(dpy, &w, &revert);
+                        if(w != root && w != 0) XDestroyWindow(dpy, w);
+                    }
+                }
             }
         }
-        else if(ev.type == ButtonPress && ev.xbutton.subwindow != None)
-        {
-            if (ev.xbutton.state & SUPER_MASK)
-            {
-                XGetWindowAttributes(dpy, ev.xbutton.subwindow, &attr);
-                start = ev.xbutton;
-            }
+        else if (ev.type == ButtonPress && ev.xbutton.subwindow != None) {
+            XGetWindowAttributes(dpy, ev.xbutton.subwindow, &attr);
+            start = ev.xbutton;
+            XRaiseWindow(dpy, start.subwindow);
+            XSetInputFocus(dpy, start.subwindow, RevertToParent, CurrentTime);
         }
-        else if(ev.type == MotionNotify && start.subwindow != None)
-        {
+        else if (ev.type == MotionNotify && start.subwindow != None) {
             int xdiff = ev.xbutton.x_root - start.x_root;
-           
-            int ydiff = ev.xbutton.y_root - start.y_root; 
-
-            XMoveResizeWindow(dpy, start.subwindow,
-                attr.x + (start.button==1 ? xdiff : 0),
-                attr.y + (start.button==1 ? ydiff : 0),
-                MAX(1, attr.width + (start.button==3 ? xdiff : 0)),
-                MAX(1, attr.height + (start.button==3 ? ydiff : 0)));
+            int ydiff = ev.xbutton.y_root - start.y_root;
+            
+            if (start.button == 1) {
+                XMoveWindow(dpy, start.subwindow, attr.x + xdiff, attr.y + ydiff);
+            } else if (start.button == 3) {
+                XResizeWindow(dpy, start.subwindow, 
+                    MAX(1, attr.width + xdiff), 
+                    MAX(1, attr.height + ydiff));
+            }
         }
-        else if(ev.type == ButtonRelease)
+        else if (ev.type == ButtonRelease) {
             start.subwindow = None;
+        }
     }
 }
